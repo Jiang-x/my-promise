c0cc9dbbcd1581598d1f3162b69bdc8626e4eb58
__Jiang <liujiangtao.x@gmail.com>
2019-01-24 14:37:19 +0800

    test

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ class MyPromise {
         let status = 'pending'
         let result = null
         this._init()
-        // 用闭包存储 promise 的状态信息，防止状态被外部修改
+        // closure
         this._getInfo = () => ({
             status,
             result
@@ -22,7 +22,7 @@ class MyPromise {
             status = s
             result = r
             this._hooks.forEach((hook) => {
-                this._asyncExec(hook, status, result)
+                setImmediate(hook, status, result)
             })
         }
         const resolve = pendingEnsure((value) => {
@@ -30,66 +30,107 @@ class MyPromise {
         })
         const reject = pendingEnsure((err) => {
             transformStatus('rejected', err)
-            this._asyncExec(() => {
+            setImmediate(() => {
                 if (!this._processed) {
                     console.warn('UnhandledPromiseRejectionWarning:', err)
+                    console.warn('DeprecationWarning: Unhandled promise rejections are deprecated. In the future, promise rejections that are not handled will terminate the Node.js process with a non-zero exit code.')
                 }
             })
         })
-        executor(resolve, reject)
+        try {
+            executor(resolve, reject)
+        } catch (e) {
+            reject(e)
+        }
     }
-    
+
     _init() {
         this._hooks = []
         this._processed = false
     }
-    
 
     _addHook(hook) {
         this._hooks.push(hook)
     }
-    
-    _asyncExec(callback, ...args) {
-        setTimeout(callback, 0, ...args)
-    }
-    
-    _execOnTransform(onTransform, result, resolve, reject) {
-        const next = onTransform(result)
-        if (next instanceof MyPromise) {
+
+    _execOnTransform(onTransform, promise, resolve, reject) {
+        const info = this._getInfo()
+        const { status, result } = info
+        let next
+        if (typeof onTransform === 'function') {
+            try {
+                next = onTransform(result)
+            } catch (e) {
+                reject(e)
+                return
+            }
+        } else {
+            if (status === 'fulfilled') {
+                resolve(result)
+            } else {
+                reject(result)
+            }
+        }
+        if (next === promise) {
+            // cause forever pending
+            throw new TypeError(`Chaining cycle detected for promise #<${this.constructor.name}>`)
+        } else if (next instanceof this.constructor) {
             next.then((value) => {
                 resolve(value)
-            }).catch((err) => {
-                reject(err)
+            }).catch((e) => {
+                reject(e)
             })
         } else {
             resolve(next)
         }
     }
 
-    then(onFulfilled) {
+    toString() {
+        return `[object ${this.constructor.name}]`
+    }
+
+    valueOf() {
+        const info = this._getInfo()
+        const { status, result } = info
+        if (status === 'pending') {
+            return `${this.constructor.name} { <pending> }`
+        } else if (status === 'fulfilled') {
+            return `${this.constructor.name} { ${result} }`
+        } else {
+            return `${this.constructor.name} { \n  <rejected> ${result} }`
+        }
+    }
+
+    // Deprecated Method: to format output with console.log in Node.js 
+    inspect() {
+        return this.valueOf()
+    }
+
+    then(onFulfilled, onRejected) {
         this._processed = true
         const info = this._getInfo()
         const { status, result } = info
         if (status === 'pending') {
-            let _resolve, _reject
+            let resolve, reject
+            const promise = new this.constructor((res, rej) => {
+                [resolve, reject] = [res, rej]
+            })
             this._addHook((status, result) => {
                 if (status === 'fulfilled') {
-                    this._execOnTransform(onFulfilled, result, _resolve, _reject)
+                    this._execOnTransform(onFulfilled, promise, resolve, reject)
                 } else {
-                    _reject(result)
+                    this._execOnTransform(onRejected, promise, resolve, reject)
                 }
             })
-            return new MyPromise((resolve, reject) => {
-                [_resolve, _reject] = [resolve , reject]
-            })
-        } else if (status === 'fulfilled') {
-            return new MyPromise((resolve, reject) => {
-                this._asyncExec(this._execOnTransform.bind(this), onFulfilled, result, resolve, reject)
-            })
+            return promise
         } else {
-            return new MyPromise((resolve, reject) => {
-                this._asyncExec(reject, result)
+            let callback = status === 'fulfilled' ? onFulfilled : onRejected
+            const promise = new this.constructor((resolve, reject) => {
+                setImmediate(() => {
+                    this._execOnTransform(callback, promise, resolve, reject)
+                })
             })
+            return promise
         }
     }
 
@@ -98,48 +139,53 @@ class MyPromise {
         const info = this._getInfo()
         const { status, result } = info
         if (status === 'pending') {
-            let _resolve, _reject
+            let resolve, reject
+            const promise = new this.constructor((res, rej) => {
+                [resolve, reject] = [res, rej]
+            })
             this._addHook((status, result) => {
                 if (status === 'fulfilled') {
-                    _resolve(result)
+                    resolve(result)
                 } else {
-                    this._execOnTransform(onRejected, result, _resolve, _reject)
+                    this._execOnTransform(onRejected, promise, resolve, reject)
                 }
             })
-            return new MyPromise((resolve, reject) => {
-                [_resolve, _reject] = [resolve , reject]
-            })
+            return promise
         } else if (status === 'fulfilled') {
-            return new MyPromise((resolve, reject) => {
-                this._asyncExec(resolve, result)
+            return new this.constructor((resolve, reject) => {
+                setImmediate(resolve, result)
             })
         } else {
-            return new MyPromise((resolve, reject) => {
-                this._asyncExec(this._execOnTransform.bind(this), onRejected, result, resolve, reject)
+            const promise = new this.constructor((resolve, reject) => {
+                setImmediate(() => {
+                    this._execOnTransform(onRejected, promise, resolve, reject)
+                })
             })
+            return promise
         }
     }
-    
+
     finally(onFinally) {
         this._processed = true
         const info = this._getInfo()
         const { status, result } = info
         if (status === 'pending') {
-            let _resolve, _reject
+            let resolve, reject
+            const promise = new this.constructor((res, rej) => {
+                [resolve, reject] = [res, rej]
+            })
             this._addHook((status, result) => {
                 onFinally()
                 if (status === 'fulfilled') {
-                    _resolve(result)
+                    resolve(result)
                 } else {
-                    _reject(result)
+                    reject(result)
                 }
             })
-            return new MyPromise((resolve, reject) => {
-                [_resolve, _reject] = [resolve , reject]
-            })
+            return promise
         } else {
-            return new MyPromise((resolve, reject) => {
-                this._asyncExec(() => {
+            return new this.constructor((resolve, reject) => {
+                setImmediate(() => {
                     onFinally()
                     if (status === 'fulfilled') {
                         resolve(result)
@@ -150,52 +196,121 @@ class MyPromise {
             })
         }
     }
+
+    static resolve(value) {
+        return new this((resolve, reject) => {
+            resolve(value)
+        })
+    }
+
+    static reject(reason) {
+        return new this((resolve, reject) => {
+            reject(reason)
+        })
+    }
+
+    static all(...args) {
+        // todo
+        const promise = new this.constructor((resolve, reject) => {
+            
+        })
+        return promise
+    }
+
+    static race(...args) {
+        // todo
+        const promise = new this.constructor((resolve, reject) => {
+            
+        })
+        return promise
+    }
 }
 
+
+
+////////////////////////////////////////////////////////////////////////////////
 
 // test
 
-const asyncExec = (cb, ...args) => {
-    setTimeout(cb, 0, ...args)
-}
-
-const p = function(ms, v, success = true) {
+const p = function(ms, v, success=true) {
     return new MyPromise((resolve, reject) => {
         setTimeout(function() {
             if (success) {
                 resolve(v)
             } else {
-                reject(new Error(v))
+                reject(v)
             }
         }, ms)
     })
 }
 
+const ensure = function(a, b, message) {
+    const r1 = JSON.stringify(a)
+    const r2 = JSON.stringify(b)
+    if (r1 !== r2) {
+        console.log(`test --> ${message} **** error ****\n  ${r1} != ${r2}`)
+    } else {
+        console.log(`test --> ${message} OK!`)
+    }
+}
 
-const x = p(1000, 'p1')
+const test1 = () => {
+    const result = []
+    p(100, 'p1').then((v) => {
+        result.push(v)
+        return p(100, 'p2', false)
+    }).then(() => {
+        result.push(null)
+        return 100
+    }).then(() => {
+        result.push(null)
+    }, (v) => {
+        result.push(v)
+        return p(100, 'p3', false)
+    }).catch((e) => {
+        result.push(e)
+        return p(100, 'p4')
+    }).finally(() => {
+        result.push('finally')
+    })
+    
+    setTimeout(ensure, 1000, result, ['p1', 'p2', 'p3', 'finally'], 'basic')
+}
 
-x.then((info) => {
-    log('info1', info)
-    return p(1000, 'p2')
-}).finally(() => {
-    log('finally 1')
-}).finally(() => {
-    log('finally 2')
-}).then((info) => {
-    log('info2', info)
-    return p(1000, 'p3')
-}).then((info) => {
-    log('info3', info)
-    return p(1000, 'p4', false)
-}).catch((err) => {
-    log('err', err)
-}).finally(() => {
-    log('finally 3')
-}).then(() => {
-    return p(12, 'error', false)
-})
+const test2 = () => {
+    const result = []
+    
+    setTimeout(ensure, 1000, result, ['p1', 'p2', 'p3', 'finally'], 'basic')
+}
 
-log('sync code done')
+const test3 = () => {
+    const result = []
+    
+    setTimeout(ensure, 1000, result, ['p1', 'p2', 'p3', 'finally'], 'basic')
+}
+
+const test4 = () => {
+    const result = []
+    
+    setTimeout(ensure, 1000, result, ['p1', 'p2', 'p3', 'finally'], 'basic')
+}
+
+const test5 = () => {
+    const result = []
+    
+    setTimeout(ensure, 1000, result, ['p1', 'p2', 'p3', 'finally'], 'basic')
+}
+
+const test = () => {
+    log('testing ...')
+    test1()
+    // test2()
+    // test3()
+    // test4()
+    // test5()
+}
+
+test()
 
 
 
